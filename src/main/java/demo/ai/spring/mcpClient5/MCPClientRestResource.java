025dbf9c1a92abd464bd1bb51e2e3241db923c2d
aandreevh <aandreevh@gmail.com>
2026-04-05 18:23:31 +0300

    Add local mcp server.

--- src/main/java/demo/ai/spring/mcpClient5/MCPClientRestResource.java
+++ src/main/java/demo/ai/spring/mcpClient5/MCPClientRestResource.java
@@ -1,39 +1,41 @@
 package demo.ai.spring.mcpClient5;
 
+import demo.ai.spring.mcpClient5.McpClientConfiguration.ToolsGroup;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import demo.ai.spring.mcpClient5.McpClientConfiguration.ToolsGroup;
-
-
+@Lazy
 @RestController
 @RequestMapping("/mcpClient")
 public class MCPClientRestResource {
-  private final ChatClient chatClient;
+    private final ChatClient chatClient;
 
-  @Autowired
-  public MCPClientRestResource(final ChatClient.Builder builder,
-      final @Qualifier("tools-file-system") ToolsGroup fileSystemTools,
-      final @Qualifier("tools-sqlite") ToolsGroup sqliteTools) {
+    @Autowired
+    public MCPClientRestResource(final ChatClient.Builder builder,
+                                 final @Qualifier("tools-file-system") ToolsGroup fileSystemTools,
+                                 final @Qualifier("tools-sqlite") ToolsGroup sqliteTools,
+                                 final @Qualifier("local-mcp-server") ToolsGroup localMcpTools) {
 
-    this.chatClient = builder
-        //register tools to be used by client
-        .defaultToolCallbacks(fileSystemTools.tools())
-        .defaultToolCallbacks(sqliteTools.tools())
-            .defaultSystem("Append every database query you do in QUERIES.md file.")
-        .build();
-  }
+        this.chatClient = builder
+                //register tools to be used by client
+                .defaultToolCallbacks(fileSystemTools.tools())
+                .defaultToolCallbacks(sqliteTools.tools())
+                .defaultToolCallbacks(localMcpTools.tools())
+                .defaultSystem("Append every database query you do in QUERIES.md file.")
+                .build();
+    }
 
-  @GetMapping
-  public String mcpClient(final @RequestBody String prompt) {
-    return chatClient.prompt()
-        .user(prompt) //TODO: Optionally register tools here
-        .call()
-        .content();
-  }
+    @GetMapping
+    public String mcpClient(final @RequestBody String prompt) {
+        return chatClient.prompt()
+                .user(prompt) //TODO: Optionally register tools here
+                .call()
+                .content();
+    }
 }
--- src/main/java/demo/ai/spring/mcpClient5/McpClientConfiguration.java
+++ src/main/java/demo/ai/spring/mcpClient5/McpClientConfiguration.java
@@ -1,20 +1,21 @@
 package demo.ai.spring.mcpClient5;
 
+import io.modelcontextprotocol.client.McpClient;
+import io.modelcontextprotocol.client.transport.HttpClientStreamableHttpTransport;
+import io.modelcontextprotocol.client.transport.ServerParameters;
+import io.modelcontextprotocol.client.transport.StdioClientTransport;
+import io.modelcontextprotocol.json.McpJsonMapper;
+import io.modelcontextprotocol.json.jackson3.JacksonMcpJsonMapper;
 import org.springframework.ai.mcp.SyncMcpToolCallback;
 import org.springframework.ai.tool.ToolCallback;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+import tools.jackson.databind.json.JsonMapper;
 
 import java.util.List;
-
-import io.modelcontextprotocol.client.McpClient;
-import io.modelcontextprotocol.client.transport.ServerParameters;
-import io.modelcontextprotocol.client.transport.StdioClientTransport;
-import io.modelcontextprotocol.json.McpJsonMapper;
-import io.modelcontextprotocol.json.jackson3.JacksonMcpJsonMapper;
-import tools.jackson.databind.json.JsonMapper;
 
 /**
  * Configure MCP Clients
@@ -22,56 +23,80 @@ import tools.jackson.databind.json.JsonMapper;
 @Configuration
 public class McpClientConfiguration {
 
-  @Bean
-  public McpJsonMapper mcpJsonMapper() {
-    //Provide json mapper required for mcp's tool input schemas
-    return new JacksonMcpJsonMapper(new JsonMapper());
-  }
+    @Bean
+    public McpJsonMapper mcpJsonMapper() {
+        //Provide json mapper required for MCP tool input schemas
+        return new JacksonMcpJsonMapper(new JsonMapper());
+    }
 
-  //TODO: explain optionally may be configured in application.yaml, for every ChatClient built
-  @Bean
-  @Qualifier("tools-file-system")
-  public ToolsGroup fileSystemTools(final McpJsonMapper mapper,
-      final @Value("${config.mcpClient.tools-file-system-directory}") String directory) {
+    @Lazy //Required as our server is not yet initialized
+    @Bean
+    @Qualifier("local-mcp-server")
+    public ToolsGroup localMcpServer(final McpJsonMapper mapper) {
+        final var transport = HttpClientStreamableHttpTransport
+                .builder("http://localhost:8080")
+                .jsonMapper(mapper)
+                .build();
 
-    //TODO: explain tool to interact with file system
-    return loadStdIOMCP(mapper,
-        "npx", "-y", "@modelcontextprotocol/server-filesystem", directory);
-  }
+        final var client = McpClient.sync(transport).build();
+        client.initialize();
 
-  @Bean
-  @Qualifier("tools-sqlite")
-  public ToolsGroup sqlite(final McpJsonMapper mapper,
-      final @Value("${config.mcpClient.tools-sqlite-db-file}") String database) {
-    //TODO: explain sqlite mcp
-    return loadStdIOMCP(mapper,
-        "npx", "-y", "mcp-server-sqlite-npx", database);
-  }
+        return new ToolsGroup(client
+                .listTools()
+                .tools()
+                .stream()
+                .map(tool -> (ToolCallback) SyncMcpToolCallback.builder()
+                        .mcpClient(client)
+                        .tool(tool)
+                        .build())
+                .toList());
+    }
 
-  private ToolsGroup loadStdIOMCP(final McpJsonMapper mapper, final String program, final String... args) {
-    //TODO: Use stdio transport based MCP -> creates a process and interacts with IO to call tools
-    final StdioClientTransport transport = new StdioClientTransport(
-        new ServerParameters.Builder(program) /*TODO: requires npx installation (explain why npx)*/
-            .args(args)
-            .build(), mapper);
+    //TODO: explain optionally may be configured in application.yaml, for every ChatClient built
+    @Bean
+    @Qualifier("tools-file-system")
+    public ToolsGroup fileSystemTools(final McpJsonMapper mapper,
+                                      final @Value("${config.mcpClient.tools-file-system-directory}") String directory) {
 
-    // Create and initialize the MCP client
-    var mcpClient = McpClient.sync(transport).build();
-    mcpClient.initialize();
+        //TODO: explain tool to interact with file system
+        return loadStdIOMCP(mapper,
+                "npx", "-y", "@modelcontextprotocol/server-filesystem", directory);
+    }
 
-    //Provide tools to interact with server filesystem
-    return new ToolsGroup(mcpClient
-        .listTools() //List all available tools
-        .tools() //Get all available tools
-        .stream()
-        .map(tool -> (ToolCallback)SyncMcpToolCallback.builder()
-            .mcpClient(mcpClient)
-            .tool(tool)
-            .build()) //Build tool callback from given tools (TODO:explain ToolCallback)
-        .toList());
-  }
+    @Bean
+    @Qualifier("tools-sqlite")
+    public ToolsGroup sqlite(final McpJsonMapper mapper,
+                             final @Value("${config.mcpClient.tools-sqlite-db-file}") String database) {
+        //TODO: explain sqlite mcp
+        //TODO: may need to change your npx path to sth like this: C:\Program Files\nodejs\npx.cmd
+        return loadStdIOMCP(mapper,
+                "npx", "-y", "mcp-server-sqlite-npx", database);
+    }
+
+    private ToolsGroup loadStdIOMCP(final McpJsonMapper mapper, final String program, final String... args) {
+        //TODO: Use stdio transport based MCP -> creates a process and interacts with IO to call tools
+        final StdioClientTransport transport = new StdioClientTransport(
+                new ServerParameters.Builder(program) /*TODO: requires npx installation (explain why npx)*/
+                        .args(args)
+                        .build(), mapper);
+
+        // Create and initialize the MCP client
+        var mcpClient = McpClient.sync(transport).build();
+        mcpClient.initialize();
+
+        //Provide tools to interact with server filesystem
+        return new ToolsGroup(mcpClient
+                .listTools() //List all available tools
+                .tools() //Get all available tools
+                .stream()
+                .map(tool -> (ToolCallback) SyncMcpToolCallback.builder()
+                        .mcpClient(mcpClient)
+                        .tool(tool)
+                        .build()) //Build tool callback from given tools (TODO:explain ToolCallback)
+                .toList());
+    }
 
 
-  public record ToolsGroup(List<ToolCallback> tools) {
-  }
+    public record ToolsGroup(List<ToolCallback> tools) {
+    }
 }
